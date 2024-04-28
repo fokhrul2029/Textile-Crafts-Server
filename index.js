603afc48317ac0a23070a62ae93bfeb17da94489
fokhrul2029 <ifokhrul.2029@gmail.com>
2024-04-29 02:37:45 +0600

    get all data of art by find method

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 require("dotenv").config();
+const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const port = process.env.PORT || 5000;
+
+// MiddleWare
+app.use(express.json());
+app.use(cors());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zebesho.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -21,9 +26,40 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
-
     app.get("/", (req, res) => {
       res.send("Hello World");
+    });
+
+    const database = client.db("TextileCrafts-Assignment-10");
+    const single_items = database.collection("all_items");
+
+    app.get("/all-data", async (req, res) => {
+      const cursor = single_items.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/", async (req, res) => {
+      const item = req.body;
+      const docs = [
+        {
+          userObj: {
+            userName: item.userName,
+            userEmail: item.userEmail,
+          },
+          image: item.image,
+          item_name: item.item_name,
+          subcategory_Name: item.subcategory_Name,
+          description: item.description,
+          price: item.price,
+          rating: item.rating,
+          customization: item.customization,
+          processing_time: item.processing_time,
+          stockStatus: item.stockStatus,
+        },
+      ];
+      const result = await single_items.insertMany(docs);
+      res.send(result);
     });
 
     await client.db("admin").command({ ping: 1 });
